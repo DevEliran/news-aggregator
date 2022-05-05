98420b459b10ed66e957d9d4dca0a443651ea85d
Eliran <eliran9692@gmail.com>
2022-05-05 18:45:35 +0300

    making it a cli, WIP

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+import argparse
+import logging
 from typing import List
 from models import  Source
 from reddit_source import RedditSource
 from medium_source import MediumSource
+import sys
 
 
 class SourceManager:
@@ -14,7 +17,35 @@ class SourceManager:
             print(source)
 
 
+def normalize_args(args):
+    if args["--reddit"] and not args["--sub"]:
+        logging.error("detected --reddit flag without --sub flag")
+        raise
+    if args["--medium"] and not args["--tag"]:
+        logging.error("detected --medium flag without --tag flag")
+        raise
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--reddit', action='store_true', type=bool)
+    parser.add_argument('--sub', action='store', type=str)
+    parser.add_argument('--metric', action='store', type=str)
+
+    parser.add_argument('--medium', action='store_true', type=bool)
+    parser.add_argument('--tag', action='store', type=str)
+
+    parser.add_argument('--limit', action='store', type=int)
+
+    try:
+        args = normalize_args(parser.parse_args())
+    except Exception:
+        logging.info("Unable to parse settings, turn on LOG_LEVEL=ERROR for a detailed log")
+        sys.exit(1)
+
+
+
+
     source_manager = SourceManager(
         [
             RedditSource(subreddit='programming', limit=10, metric='top'),
