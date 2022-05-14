1c4fa6cc5211117c0562dab1ccb2d6965f005015
Eliran <eliran9692@gmail.com>
2022-05-14 10:38:58 +0300

    adding docs to reddit source

--- src/reddit_source.py
+++ src/reddit_source.py
@@ -1,20 +1,49 @@
-from src.models import Source, Result
+import os
 from typing import List
 import praw
 from praw.reddit import Reddit
-import os
 from colorama import Fore, Style
+
+from src.models import Source, Result
 
 CLIENT_ID = os.environ.get('REDDIT_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('REDDIT_CLIENT_SECRET')
 
+REDDIT_VALID_METRICS = ['hot', 'top']
+
+"""
+RedditSource module, responsible for querying Reddit's API.
+"""
+
+
+def reformat_results(raw_results) -> List[Result]:
+    """
+    Transforming Reddit's API raw results into the Result class
+    """
+    reformatted_results = []
+    for result in raw_results:
+        reformatted_results.append(
+            Result(
+                title=vars(result)['title'],
+                url=vars(result)['url']
+            )
+        )
+    return reformatted_results
+
 
 class RedditSource(Source):
-
+    """
+    RedditSource class accepts a subreddit, metric, limit and credentials
+    for API access.
+    Queries Reddit's API based on the subreddit and metric specified.
+    """
     def __init__(self, subreddit: str, limit: int = 10, metric: str = 'hot',
                  reddit_id: str = None, reddit_secret: str = None) -> None:
+        """
+        Initializes the empty results list, connection to the API and given
+        attributes.
+        """
         self.results: List[Result] = []
-        self.valid_metrics = ['hot', 'top']
         self.subreddit = subreddit
         self.limit = limit
         self.metric = metric
@@ -23,8 +52,12 @@ class RedditSource(Source):
         self.reddit_con = self.connect()
 
     def connect(self) -> Reddit:
+        """
+        Connects to Reddit's API with the credentials given.
+        """
         reddit_id = self.reddit_id if self.reddit_id else CLIENT_ID
-        reddit_secret = self.reddit_secret if self.reddit_secret else CLIENT_SECRET
+        reddit_secret = self.reddit_secret if \
+            self.reddit_secret else CLIENT_SECRET
 
         self.reddit_con = praw.Reddit(client_id=reddit_id,
                                       client_secret=reddit_secret,
@@ -33,8 +66,11 @@ class RedditSource(Source):
         return self.reddit_con
 
     def fetch(self) -> List[Result]:
-        if not self.subreddit or self.limit < 0 or\
-                self.metric.lower() not in self.valid_metrics:
+        """
+        Retrieves posts from Reddit's API based on the subreddit and metric.
+        """
+        if not self.subreddit or self.limit < 0 or \
+                self.metric.lower() not in REDDIT_VALID_METRICS:
             return []
 
         if self.metric == 'hot':
@@ -44,22 +80,14 @@ class RedditSource(Source):
             raw_results = self.reddit_con.subreddit(self.subreddit).top(
                 limit=self.limit)
 
-        self.results = self.reformat_results(raw_results)
+        self.results = reformat_results(raw_results)
 
         return self.results
 
-    def reformat_results(self, raw_results) -> List[Result]:
-        reformatted_results = []
-        for result in raw_results:
-            reformatted_results.append(
-                Result(
-                    title=vars(result)['title'],
-                    url=vars(result)['url']
-                )
-            )
-        return reformatted_results
-
     def __repr__(self) -> str:
+        """
+        RedditSource string representation
+        """
         output = f"{Fore.GREEN}Reddit Source Results [Sub: {self.subreddit}," \
                  f" Metric: {self.metric}]{Style.RESET_ALL} \n"
         for result in self.results:
