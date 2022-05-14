cf85d601a4f65436cc1308cd590fbe665c41d53e
Eliran <eliran9692@gmail.com>
2022-05-14 10:22:46 +0300

    adding docs to medium source module

--- src/medium_source.py
+++ src/medium_source.py
@@ -1,41 +1,68 @@
 from typing import List
-from src.models import Source, Result
 import feedparser
 from colorama import Fore, Style
+from src.models import Source, Result
+
+
+"""
+MediumSource module, responsible for querying Medium feed based on tags.
+"""
+
+
+def reformat_results(raw_results) -> List[Result]:
+    """
+    Transforming feedparser raw results into the Result class
+    """
+    results = []
+    for result in raw_results:
+        results.append(
+            Result(
+                title=result.title,
+                url=result.link
+            )
+        )
+
+    return results
 
 
 class MediumSource(Source):
+    """
+    MediumSource class accepts a tag and a limit.
+    Parsing Medium's feed based on the tag and limit specified.
+    """
     
     def __init__(self, tag, limit=10) -> None:
+        """
+        Initiate empty results list
+        """
         self.results: List[Result] = []
         self.tag = tag
         self.limit = limit
 
     def connect(self):
+        """
+        Medium doesn't require an API key, hence there's no need to
+        implement `connect`
+        """
         pass
 
     def fetch(self) -> List[Result]:
+        """
+        Retrieves posts from Medium feed based on the tag specified.
+        """
         if not self.tag or self.limit < 0:
             return []
 
-        raw_results = feedparser.parse(f"https://medium.com/feed/tag/{self.tag}").entries[:self.limit]
+        raw_results = feedparser.parse(f"https://medium.com/feed/tag/"
+                                       f"{self.tag}").entries[:self.limit]
 
-        self.results = self.reformat_results(raw_results)
+        self.results = reformat_results(raw_results)
         return self.results
 
-    def reformat_results(self, raw_results) -> List[Result]:
-        results = []
-        for result in raw_results:
-            results.append(
-                Result(
-                    title=result.title,
-                    url=result.link
-                )
-            )
-
-        return results
-
     def __repr__(self) -> str:
+        """
+        MediumSource string representation
+        """
         output = f"{Fore.GREEN}Medium Source Results" \
                  f" [Tag: {self.tag}]{Style.RESET_ALL} \n"
         for result in self.results:
